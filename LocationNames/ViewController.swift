195696ddafc54680017654e7b96ec0e330fc34d7
Adriancys Jesus Villegas Toro <adriancysvillegast@gmail.com>
2022-08-05 11:02:17 -0400

    Initial Commit

---
+++ LocationNames/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationNames
+//
+//  Created by Adriancys Jesus Villegas Toro on 5/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
